7e227e11a4db84488e1008dce471fd904cbdf3ab
fpemud <fpemud@sina.com>
2016-05-09 17:22:03 +0800

    fix

--- lib/fwp_daemon.py
+++ lib/fwp_daemon.py
@@ -6,6 +6,7 @@ import sys
 import signal
 import shutil
 import logging
+import subprocess
 from gi.repository import GLib
 from fwp_util import FwpUtil
 from fwp_util import StdoutRedirector
@@ -15,6 +16,7 @@ class FwpDaemon:
 
     def __init__(self, param):
         self.param = param
+        self.apacheProc = None
 
     def run(self):
         FwpUtil.mkDirAndClear(self.param.tmpDir)
@@ -24,6 +26,10 @@ class FwpDaemon:
 
             logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
             logging.getLogger().setLevel(logging.INFO)
+
+            # start apache
+            self.apacheProc = self._runApache()
+            logging.info("apache started.")
 
             # write pid file
             with open(os.path.join(self.param.tmpDir, "fpemud-portal.pid"), "w") as f:
@@ -47,3 +53,60 @@ class FwpDaemon:
     def _sigHandlerTERM(self, signum):
         logging.info("SIGTERM received.")
         self.param.mainloop.quit()
+
+    def _runApache(self):
+        # make apache root directory
+        apacheDir = os.path.join(self.param.tmpDir, "apache") 
+        os.mkdir(apacheDir)
+
+        # generate apache config file
+        buf = ""
+        buf += "LoadModule log_config_module      /usr/lib/apache2/modules/mod_log_config.so\n"
+        buf += "LoadModule env_module             /usr/lib/apache2/modules/mod_env.so\n"
+        buf += "LoadModule unixd_module           /usr/lib/apache2/modules/mod_unixd.so\n"	
+        buf += "LoadModule alias_module           /usr/lib/apache2/modules/mod_alias.so\n"
+        buf += "LoadModule cgi_module             /usr/lib/apache2/modules/mod_cgi.so\n"
+        buf += "\n"
+        buf += "LoadModule ssl_module             /usr/lib/apache2/modules/mod_ssl.so\n"
+        buf += "LoadModule auth_basic_module      /usr/lib/apache2/modules/mod_auth_basic.so\n"
+        buf += "LoadModule authn_core_module      /usr/lib/apache2/modules/mod_authn_core.so\n"
+        buf += "LoadModule authn_file_module      /usr/lib/apache2/modules/mod_authn_file.so\n"
+        buf += "LoadModule authz_core_module      /usr/lib/apache2/modules/mod_authz_core.so\n"
+        buf += "LoadModule authz_user_module      /usr/lib/apache2/modules/mod_authz_user.so\n"
+        buf += "\n"
+        buf += "\n"
+        buf += "ServerName $(hostname)\n"        
+        buf += "DocumentRoot \"%s\"\n" % (self.param.shareDir)        
+        buf += "\n"        
+        buf += "PidFile \"%s\"\n" % (os.path.join(apacheDir, "apache.pid"))	
+        buf += "ErrorLog \"%s\"\n" % (os.path.join(apacheDir, "error.log"))
+        buf += "LogFormat \"%h %l %u %t \\\"%r\\\" %>s %b \\\"%{Referer}i\\\" \\\"%{User-Agent}i\\\"\" common\n"
+        buf += "CustomLog \"%s\" common\n" % (os.path.join(apacheDir, "access.log"))
+        buf += "\n"
+        buf += "User fpemud-portal\n"
+        buf += "Group fpemud-portal\n"
+        buf += "\n"
+        buf += "Listen %d https\n" % (self.param.listenPort)
+        buf += "\n"
+        buf += "SSLEngine on\n"        
+        buf += "SSLProtocol all\n"
+        buf += "SSLCertificateFile \"%s\"\n" % (self.param.certFile)
+        buf += "SSLCertificateKeyFile \"%s\"\n" % (self.param.keyFile)
+        buf += "\n"
+        buf += "<Directory \"%s\">\n" % (self.param.shareDir)
+        buf += "    AuthType Basic\n"
+        buf += "    AuthName \"Web Portal for Fpemud\"\n"
+        buf += "    AuthBasicProvider file\n"
+        buf += "    AuthUserFile \"%s\"\n" % (self.param.htpasswdFile)
+        buf += "    Require valid-user\n"
+        buf += "</Directory>\n"
+        buf += "\n"
+        cfgf = os.path.join(apacheDir, "apache.conf")
+        with open(cfgf, "w") as f:
+            f.write(buf)
+
+        # run apache process
+        cmd = "/usr/sbin/apache2 -d \"%s\" -f \"%s\" -DFOREGROUND" % (apacheDir, cfgf)
+        proc = subprocess.Popen(cmd, shell=True, universal_newlines=True)
+
+        return proc
--- lib/fwp_param.py
+++ lib/fwp_param.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
+import os
+
 
 class FwpParam:
 
     def __init__(self):
         self.libDir = "/usr/lib/fpemud-portal"
+        self.shareDir = "/usr/share/fpemud-portal"
+        self.varDir = "/var/fpemud-portal"
         self.tmpDir = "/tmp/fpemud-portal"
+
+        self.certFile = os.path.join(self.varDir, "cert.pem")
+        self.keyFile = os.path.join(self.varDir, "prikey.pem")
+        self.htpasswdFile = os.path.join(self.VarDir, "htpasswd")
+
+        self.listenPort = 443
 
         self.mainloop = None
